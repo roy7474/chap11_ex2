e38fd13b259e705a834a2f1e4f43ecf58f092e38
Roy Contreras Benitez <roy7474@gmail.com>
2023-08-27 21:16:14 -0400

    main part of the program has been created

---
+++ chap11_ex2.py
@@ -0,0 +1,20 @@
+'''Exercise 2: Write a program to look for lines of the form:
+
+New Revision: 39772
+
+Extract the number from each of the lines using a regular expression and 
+the findall() method. Compute the average of the numbers and print out the
+ average as an integer.
+Enter file:mbox.txt
+38549
+
+Enter file:mbox-short.txt
+39756'''
+
+import re
+fhand = open('mbox-short.txt')
+
+for line in fhand:
+    words = line.rstrip()
+    x = re.findall('^New Revision: .*([0-9])', words)
+    print(x)
